04742a1ebac05fcbe0442ff2b7b2d1fc2f6be742
JovanMoolah <145720096+JovanMoolah@users.noreply.github.com>
2025-03-12 01:02:47 -0400

    Update GesturetoSpeechTrainModel.py

--- GesturetoSpeechTrainModel/GesturetoSpeechTrainModel/GesturetoSpeechTrainModel.py
+++ GesturetoSpeechTrainModel/GesturetoSpeechTrainModel/GesturetoSpeechTrainModel.py
@@ -1,11 +1,6 @@
-
 import ultralytics
-
 from ultralytics import YOLO
-
 import torch
-
-
 
 #Checks if CUDA GPU is available
 #If GPU is not availabel, download Torch libaray instead to use cpu for training
@@ -16,13 +11,10 @@ else:
     print("GPU is not available")   
 '''
 
-
 #Trains pretrained model with dataset
 model = YOLO('yolov8x-pose.yaml').load('yolov8x-pose.pt')
 if __name__ == '__main__':
     results = model.train(data=r'F:\kjldovpnqbd38out\data.yaml', epochs=75, imgsz=576, plots=True, batch=16, translate = 0, erasing = 0, scale =1) 
-
-
 
 #Resume training or validate model
 '''
